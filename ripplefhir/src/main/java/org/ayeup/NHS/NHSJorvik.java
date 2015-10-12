2624700cb982444d058ada0b0c31b48161900f45
KevinMayfield <mayfield.g.kev@gmail.com>
2015-10-12 20:04:31 +0100

    Added search parameters and tidied comments

--- ripplefhir/src/main/java/org/ayeup/NHS/NHSJorvik.java
+++
@@ -1,6 +0,0 @@
-package org.ayeup.NHS;
-
-public class NHSJorvik {
-	 public final static String URI_NHS_ACUTE_HOSPITAL_NUMBER = "http://jorvik.nhs.uk/FHIR/Patient/HospitalNumber";
-	 public final static String URI_NHS_ACUTE_EDMS_ID = "http://jorvik.nhs.uk/FHIR/DocumentReference/EDMSNumber";
-}
--- ripplefhir/src/main/java/org/ayeup/NHS/NHSUK.java
+++
@@ -1,7 +0,0 @@
-package org.ayeup.NHS;
-
-public class NHSUK
-{
-		public final static String URI_CODE_SYSTEM_SNOMED_CT = "http://snomed.info/sct";
-	    	
-}
