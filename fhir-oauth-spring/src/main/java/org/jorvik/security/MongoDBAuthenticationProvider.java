00bf25b0db1f432073abc098adb235a6a140b3c7
kevin.mayfield <kevin.mayfield@this.nhs.uk>
2015-11-03 11:01:04 +0000

    Pre OAuth2 change backup

--- fhir-oauth-spring/src/main/java/org/jorvik/security/MongoDBAuthenticationProvider.java
+++ fhir-oauth-spring/src/main/java/org/jorvik/security/MongoDBAuthenticationProvider.java
@@ -4,6 +4,7 @@ import org.jorvik.dao.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -47,7 +48,7 @@ public class MongoDBAuthenticationProvider implements UserDetailsService {
 	}
 	
 	@Autowired
-	 public void setMongoTemplate(MongoTemplate mongoTemplate) {
+	 public void setMongoTemplate(@Qualifier("mongoTemplate") MongoTemplate mongoTemplate) {
 	 this.mongoTemplate = mongoTemplate;
 	 }
 	
