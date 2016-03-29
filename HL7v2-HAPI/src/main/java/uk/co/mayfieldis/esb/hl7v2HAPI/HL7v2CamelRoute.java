365b9e2beb462a486a0d7eeeeeb02b12e698d7f1
kevin.mayfield <kevin.mayfield@this.nhs.uk>
2016-03-29 16:49:20 +0100

    Testing for posting to live HAPI

--- HL7v2-HAPI/src/main/java/uk/co/mayfieldis/esb/hl7v2HAPI/HL7v2CamelRoute.java
+++ HL7v2-HAPI/src/main/java/uk/co/mayfieldis/esb/hl7v2HAPI/HL7v2CamelRoute.java
@@ -6,11 +6,16 @@ import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HapiContext;
 import uk.co.mayfieldis.hl7v2.hapi.processor.ADTA28A31toPatient;
 import uk.co.mayfieldis.hl7v2.hapi.processor.EnrichMFNM05withLocation;
+import uk.co.mayfieldis.hl7v2.hapi.processor.EnrichPatientwithOrganisation;
+import uk.co.mayfieldis.hl7v2.hapi.processor.EnrichPatientwithPatient;
+import uk.co.mayfieldis.hl7v2.hapi.processor.EnrichPatientwithPractitioner;
 import uk.co.mayfieldis.hl7v2.hapi.processor.LightWithFHIR;
 import uk.co.mayfieldis.hl7v2.hapi.processor.MFNM02PractitionerProcessor;
 import uk.co.mayfieldis.hl7v2.hapi.processor.MFNM05LocationProcessor;
 
 import static org.apache.camel.component.hl7.HL7.ack;
+
+import org.apache.camel.Exchange;
 
 
 public class HL7v2CamelRoute extends RouteBuilder {
@@ -26,13 +31,18 @@ public class HL7v2CamelRoute extends RouteBuilder {
     	
     	hl7.setHapiContext(hapiContext);
     	
-    	LightWithFHIR lightWithFHIR = new LightWithFHIR(); 
+    	//LightWithFHIR lightWithFHIR = new LightWithFHIR(); 
     	EnrichMFNM05withLocation enrichMFNM05withLocation = new EnrichMFNM05withLocation();
     	ADTA28A31toPatient adta28a31toPatient = new ADTA28A31toPatient();  
     	MFNM02PractitionerProcessor mfnm02PractitionerProcessor = new MFNM02PractitionerProcessor();
     	MFNM05LocationProcessor mfnm05LocationProcessor = new MFNM05LocationProcessor();
+    	EnrichPatientwithOrganisation enrichPatientwithOrganisation = new EnrichPatientwithOrganisation();
+    	EnrichPatientwithPractitioner enrichPatientwithPractitioner = new EnrichPatientwithPractitioner();
+    	EnrichPatientwithPatient enrichPatientwithPatient = new EnrichPatientwithPatient();
     	
-    	onException(org.apache.camel.CamelAuthorizationException.class)
+    	onException(org.apache.
+    			camel.CamelAuthorizationException.class)
+    		.routeId("Error")
     		.handled(true)
     		.to("log:uk.co.mayfieldis.hl7v2.hapi.route.processor.OperationOutcome?level=WARN")
     		.to("bean:outcome?method=getOutcome(401,'Unauthorized'+${exception.policyId}, '')");
@@ -42,13 +52,19 @@ public class HL7v2CamelRoute extends RouteBuilder {
     		.unmarshal(hl7)
     		//.process("HL7v2Service")
     		.choice()
-				.when(header("CamelHL7MessageType").isEqualTo("ADT")).enrich("vm:ADT",lightWithFHIR)
-				//.when(header("CamelHL7MessageType").isEqualTo("ORM")).to("vm:ORM")
-				//.when(header("CamelHL7MessageType").isEqualTo("ORU")).to("vm:ORU")
-				.when(header("CamelHL7MessageType").isEqualTo("MFN")).enrich("vm:MFN",lightWithFHIR)
+				.when(header("CamelHL7MessageType").isEqualTo("ADT"))
+					.wireTap("vm:ADT")
+					.end()
+				.when(header("CamelHL7MessageType").isEqualTo("MFN"))
+					.wireTap("vm:MFN")
+					.end()
 			.end()
     		.transform(ack());
-    	
+    	/*
+    	 * 
+    	 * //.when(header("CamelHL7MessageType").isEqualTo("ORM")).to("vm:ORM")
+				//.when(header("CamelHL7MessageType").isEqualTo("ORU")).to("vm:ORU")
+    	 * 
     	from("vm:ORM")
     		.routeId("ORM")
     		.log("ORM");
@@ -56,73 +72,78 @@ public class HL7v2CamelRoute extends RouteBuilder {
     	from("vm:ORU")
 			.routeId("ORU")
 			.log("ORU");
-
-    	from("vm:MFN")
-			.routeId("MFN")
-			.choice()
-				.when(header("CamelHL7TriggerEvent").isEqualTo("M02")).to("vm:MFN_M02")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("M05")).to("vm:MFN_M05")
-			.end();
+		*/
+    	from("vm:ADT?waitForTaskToComplete=Never")
+    		.routeId("ADT")
+    		.choice() //enrich("vm:ADT",lightWithFHIR)
+    			.when(header("CamelHL7TriggerEvent").isEqualTo("A28")).to("activemq:ADT_A28A31")
+    			.when(header("CamelHL7TriggerEvent").isEqualTo("A31")).to("activemq:ADT_A28A31")
+    			.when(header("CamelHL7TriggerEvent").isEqualTo("A40")).to("activemq:ADT_A40")
+    		.end();
     	
-    	from("vm:MFN_M02")
+    	from("vm:MFN?waitForTaskToComplete=Never")
+    		.routeId("MFN")
+    		.choice()
+    			.when(header("CamelHL7TriggerEvent").isEqualTo("M02")).to("activemq:MFN_M02")
+    			.when(header("CamelHL7TriggerEvent").isEqualTo("M05")).to("activemq:MFN_M05")
+    		.end();
+    	
+    	from("activemq:MFN_M02")
 			.routeId("MFN_M02")
 			.process(mfnm02PractitionerProcessor)
-			.to("vm:FileFHIR");
+			.to("activemq:FileFHIR");
 	
-		from("vm:MFN_M05")
+		from("activemq:MFN_M05")
 			.routeId("MFN_M05")
 			.process(mfnm05LocationProcessor)
 			.enrich("vm:Location",enrichMFNM05withLocation)
-			.to("vm:FileFHIR");
-	    	
-    	from("vm:ADT")
-    		.routeId("ADT")
-    		.choice()
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A01")).to("vm:ADT_A01")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A04")).to("vm:ADT_A04")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A05")).to("vm:ADT_A05")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A28")).to("activemq:ADT_A28A31")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A31")).to("activemq:ADT_A28A31")
-				.when(header("CamelHL7TriggerEvent").isEqualTo("A40")).to("vm:ADT_A40")
-			.end();
-    	
-    	from("vm:ADT_A01")
-			.routeId("ADT_A01")
-			.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true");
-    	
-    	from("vm:ADT_A04")
-			.routeId("ADT_A04")
-			.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true");
-    
-    	from("vm:ADT_A05")
-			.routeId("ADT_A05")
-			.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true");
-
-    	
-    	from("activemq:ADT_A28A31")
+			.to("activemq:FileFHIR");
+	    
+		from("activemq:ADT_A28A31")
 			.routeId("ADT_A28A31")
 			.process(adta28a31toPatient)
 			.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true")
-			.to("vm:FileFHIR");
+			.enrich("vm:Organisation",enrichPatientwithOrganisation)
+			.enrich("vm:Practitioner",enrichPatientwithPractitioner)
+			.enrich("vm:Patient",enrichPatientwithPatient)
+			.to("log:uk.co.mayfieldis.hl7v2.hapi.route?showAll=true&multiline=true")
+			.to("activemq:FileFHIR");
     	    	
-    	from("vm:ADT_A40")
+    	from("activemq:ADT_A40")
     		.routeId("ADT_A40")
     		.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true");
-         
-    	from("vm:Location")
-    		.transform(constant("Move along. Nothing to see here"));
-    	
-    	from("vm:FileFHIR")
-    		.to("log:uk.co.mayfieldis.hl7v2.hapi.route.HL7v2CamelRoute?showAll=true&multiline=true")
+            	
+    	from("activemq:FileFHIR")
+    		.routeId("FileStore")
     		.to("file:C:/NHSSDS/fhir?fileName=${date:now:yyyyMMdd hhmm.ss} ${header.CamelHL7MessageControl}.xml");
     	
     	from("vm:HAPIFHIR")
     		.routeId("HAPI FHIR")
-    		.to("log:uk.co.mayfieldis.hl7v2.hapi.route?showAll=true&multiline=true")
-    		//&amp;bridgeEndpoint=true
     		.to("http:chft-ddmirth.xthis.nhs.uk:8181/hapi-fhir-jpaserver/baseDstu2?connectionsPerRoute=60");
 		
-    		
-		
+    	from("vm:Organisation")
+	    	.routeId("Lookup FHIR Organisation")
+	    	.setBody(simple(""))
+	    	.setHeader(Exchange.HTTP_METHOD, simple("GET", String.class))
+	    	.setHeader(Exchange.HTTP_PATH, simple("/Organization",String.class))
+	    	.setHeader(Exchange.HTTP_QUERY,simple("identifier=urn:fhir.nhs.uk/id/ODSOrganisationCode|${header.FHIROrganisationCode}",String.class))
+	    	.to("vm:HAPIFHIR");
+    	
+    	from("vm:Practitioner")
+    		.routeId("Lookup FHIR Practitioner")
+	    	.setBody(simple(""))
+	    	.setHeader(Exchange.HTTP_METHOD, simple("GET", String.class))
+	    	.setHeader(Exchange.HTTP_PATH, simple("/Practitioner",String.class))
+	    	.setHeader(Exchange.HTTP_QUERY,simple("identifier=urn:fhir.nhs.uk/id/GeneralPractitionerPPDCode|${header.FHIRGP}",String.class))
+	    	.to("vm:HAPIFHIR");
+    	
+    	from("vm:Patient")
+			.routeId("Lookup FHIR Patient")
+			.setBody(simple(""))
+			.setHeader(Exchange.HTTP_METHOD, simple("GET", String.class))
+	    	.setHeader(Exchange.HTTP_PATH, simple("/Patient",String.class))
+	    	.setHeader(Exchange.HTTP_QUERY,simple("identifier=http://fhir.chft.nhs.uk/DistrictNumber|${header.FHIRPatient}",String.class))
+	    	.to("vm:HAPIFHIR");
+
     }
 }
